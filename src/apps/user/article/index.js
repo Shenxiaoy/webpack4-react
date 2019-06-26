6b6d2129e4d2ca5cff989df971d581700aaebb58
shenxiaoyu <shenxiaoyugaosiedu.com>
2019-06-26 10:10:39 +0800

    test react method

--- src/apps/user/article/index.js
+++ src/apps/user/article/index.js
@@ -3,12 +3,25 @@ import React from 'react'
 export default class Article extends React.Component {
   constructor() {
     super()
+    this.state = {
+      num: 1
+    }
   }
 
+  static getDerivedStateFromProps(preProps, preState) {
+    console.log('derived')
+    return {num: 'dd'}
+  }
+
+  click = (e) => {
+    this.setState({num: this.state.num+1})
+  }
 
   render() {
+    console.log(111, this.state.num)
     return <div>
-      Article
+      Article {this.state.num}
+      <p><button onClick={this.click}>点击</button></p>
     </div>
   }
 }
